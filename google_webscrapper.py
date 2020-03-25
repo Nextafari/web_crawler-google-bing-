da3a1e0595912fee0ba17bf23616760e450cf970
Nextafari <chinedue16@gmail.com>
2020-03-25 06:54:00 +0100

    commented codes

--- google_webscrapper.py
+++ google_webscrapper.py
@@ -9,18 +9,20 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 headers = {"user-agent": USER_AGENT}    #mimicks a client's request from a browser and prevents being blocked off webscrapping
 r = requests.get(url, params=params, headers=headers)   #request (.get) method
 
-soup = BeautifulSoup(r.content, "html.parser")  #
-results = soup.find("div", {"id": "search"})
-links = results.find_all("div", {"class": "r"})
+soup = BeautifulSoup(r.content, "html.parser")  #the first argument is the content from the get request, and "html.parser" 
+results = soup.find("div", {"id": "search"})    #variable for html element housing search results with values (html tags/id/class) passed as args of ("strings, and dict")
+links = results.find_all("div", {"class": "r"}) #variable for html tag housing the content(weblinks, news, etc) with attributes of the html elements as arguments of ("strings, and dict")
 
 
+#A function to crawl the web results from google
 
 def google_webcrawler():
-    for i in links:
-        news_title = i.find("a").text
-        news_link = i.find('a').attrs["href"]
+    #looping through the results of the variable links
+    for i in links:     
+        news_title = i.find("a").text #a variable for holding and converting to text from links above
+        news_link = i.find('a').attrs["href"] #a variable for holding the weblink gotten from "links" above
 
-        if news_title and news_link:
+        if news_title and news_link:        #conditional for printing_news title and news_link
            print(news_title)
            print(news_link)       
 google_webcrawler()
